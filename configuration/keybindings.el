063ae7833bfec70a0d1c83847a391e1fcb661884
danShumway <shumway.danny@gmail.com>
2017-06-14 23:28:22 -0400

    keybindings: organization and additions
     - jump command
     - better undo/redo
     - open TODO

--- configuration/keybindings.el
+++ configuration/keybindings.el
@@ -2,10 +2,24 @@
 ;;This won't override vim bindings, which I'm fine with. I'll do that below.
 (cua-mode t)
 
+;;jump command: SPC-SPC
+(spacemacs/set-leader-keys "SPC" 'evil-avy-goto-word-or-subword-1)
 
+;;better undo/redo: u/U
+(define-key evil-motion-state-map (kbd "u") 'undo)
+(define-key evil-motion-state-map (kbd "U") 'undo-tree-redo)
+
+;;save: ctrl-s - likely unnecessary, I don't really use it at this point
 (global-unset-key (kbd "C-s"))
-(global-set-key (kbd "C-s") 'save-buffer);; save: ctrl-s
+(global-set-key (kbd "C-s") 'save-buffer)
 
+;;open org base: SPC-o-o - TODO: defun a function so that documentation shows up in editor
+(spacemacs/set-leader-keys "oo" (lambda () (interactive)
+                                  (find-file "~/Dropbox/Org/TODO.org")))
+
+
+;;--------------none of the below is really working-------------------------
+;;--------------it's also not immediately clear why it needs to-------------
 
 ;;(global-set-key (kbd "C-S-s") 'write-file);; save-as: ctrl-S
 
